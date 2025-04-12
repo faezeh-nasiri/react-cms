ea18f064fc030076a6f45ea1b94ec5cc09c0b7e2
faezeh-nasiri <faezeh.n2112@gmail.com~>
2025-04-12 10:40:49 +0330

    add comment section

--- frontend/src/Component/DeleteModal/DeleteModal.js
+++ frontend/src/Component/DeleteModal/DeleteModal.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react'
 import ReactDom from 'react-dom';
 import './DeleteModal.css';
 
-export default function DeleteModal({submitAction, cancelAction}) {
+export default function DeleteModal({submitAction, cancelAction, title}) {
+
+    useEffect(() => {
+        const checkKey = (event) => {
+            if(event.keyCode === 27) {
+                cancelAction()
+            }
+        }
+
+        window.addEventListener('keydown', checkKey)
+
+        return () => window.removeEventListener('keydown', checkKey)
+    })
+
     return ReactDom.createPortal(
         <div className="modal-parent active">
             <div className="delete-modal">
-                <h1>آیا از حذف محصول اطمینان دارید؟</h1>
+                <h1>{title}</h1>
                 <div className="delete-modal-btns">
                     <button className='delete-btn delete-modal-accept-btn' onClick={() => submitAction()}>بله</button>
                     <button className='delete-btn delete-modal-reject-btn' onClick={() => cancelAction()}>خیر</button>
